f46e11e4614978e525ead8740c5cc56419976e2e
ykn9080 <ykn9080@empal.com>
2023-07-28 14:27:05 +0900

    output:export delete

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    // experimental: {
-    //     appDir: true,
-    //   },
-    output: "export"
+    experimental: {
+        appDir: true,
+      },
+    // output: "export"
 
 }
 
